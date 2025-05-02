9b7597fe9871ae23c3519193c4133f8ed9d57dc7
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-05-02 04:07:51 +0100

    chore(ui): changed theme color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        default: "#e97a56"
+      },
       fontFamily: {},
       animation: {},
     },
