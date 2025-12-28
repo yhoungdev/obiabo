6c4791983937cbe6376b97a0e3b3459ef32e9cf3
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-12-28 21:06:30 +0100

    chore: setup sanity CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,18 @@ import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 
 
+import sanity from "@sanity/astro";
+
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), sanity({
+    projectId: '6jsm7r00',
+    dataset: 'production',
+    // Optionally, you can specify other Sanity options here
+    useCdn: false,
+  })],
 
   adapter: vercel({
     webAnalytics: {
